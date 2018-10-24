bac9e2a74245b17a3b1d6246efd8a58a41e4678f
heather sterman <heather.sterman531@gmail.com>
2018-10-24 14:57:09 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -11,6 +11,16 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  ttotal = 0 
+  total = 0 
   consolidated = consolidate_cart(cart)
+  coupons = apply_coupons(consolidated, coupons)
+  apply_clearance = apply_clearance(coupons)
+  binding.pry 
+  cart.each do |hash|
+    hash.each do |fruit, attributes|
+total += attributes{:price}
 end
+end 
+total
+end
+
